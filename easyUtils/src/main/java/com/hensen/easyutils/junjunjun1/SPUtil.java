d1a1c1f9b6d086d640fa2a5aac21ec16167e0149
xuyingjun <xuyingjun@yy.com>
2021-11-22 10:10:51 +0800

    1.0.4

--- easyUtils/src/main/java/com/hensen/easyutils/junjunjun1/SPUtil.java
+++ easyUtils/src/main/java/com/hensen/easyutils/junjunjun1/SPUtil.java
@@ -2,6 +2,9 @@ package com.hensen.easyutils.junjunjun1;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
 
 /**
  * @Desc:
@@ -30,62 +33,55 @@ public class SPUtil {
         mSp = context.getSharedPreferences("config", Context.MODE_PRIVATE);
         mEditor = mSp.edit();
     }
-    /*--------------- 读 ---------------*/
 
-    /**
-     * 读取字符信息
-     *
-     * @param key
-     * @param defValue
-     * @return
-     */
+    public <T> T getObject(Class<T> clazz) {
+        String key = clazz.getName();
+        String json = getString(key, null);
+        if (TextUtils.isEmpty(json)) {
+            return null;
+        }
+        try {
+            Gson gson = new Gson();
+            return gson.fromJson(json, clazz);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public void putObject(Object object) {
+        String key = object.getClass().getName();
+        Gson gson = new Gson();
+        String json = gson.toJson(object);
+        putString(key, json);
+    }
+
+    public void removeObject(Class<?> clazz) {
+        mEditor.remove(clazz.getName());
+        mEditor.commit();
+    }
+
     public String getString(String key, String defValue) {
         return mSp.getString(key, defValue);
     }
 
-    /**
-     * 读取整型
-     */
     public int getInt(String key, int defValue) {
         return mSp.getInt(key, defValue);
     }
 
-    /**
-     * 读取布尔型
-     */
     public boolean getBoolean(String key, boolean defValue) {
         return mSp.getBoolean(key, defValue);
     }
-    /*--------------- 写 ---------------*/
 
-    /**
-     * 写入string
-     *
-     * @param key
-     * @param value
-     */
     public void putString(String key, String value) {
         mEditor.putString(key, value);
         mEditor.commit();
     }
 
-    /**
-     * 写入int
-     *
-     * @param key
-     * @param value
-     */
     public void putInt(String key, int value) {
         mEditor.putInt(key, value);
         mEditor.commit();
     }
 
-    /**
-     * 写入boolean
-     *
-     * @param key
-     * @param value
-     */
     public void putBoolean(String key, boolean value) {
         mEditor.putBoolean(key, value);
         mEditor.commit();
---
+++ easyUtils/src/main/java/com/hensen/easyutils/junjunjun1/data/Fruit.kt
@@ -0,0 +1,47 @@
+package com.hensen.easyutils.junjunjun1.data
+
+import java.util.*
+
+/**
+ * @Desc:
+ * @Author: xuyingjun
+ * @Date: 2021/11/22.
+ * @Email: xuyingjun@yy.com
+ */
+object Fruit {
+    var fruits: ArrayList<String> = ArrayList()
+
+    init {
+        fruits.add("苹果")
+        fruits.add("凤梨")
+        fruits.add("梨")
+        fruits.add("香蕉")
+        fruits.add("葡萄")
+        fruits.add("黑加仑")
+        fruits.add("葡萄")
+        fruits.add("桃子")
+        fruits.add("橙子")
+        fruits.add("樱桃")
+        fruits.add("西瓜")
+        fruits.add("蓝莓")
+        fruits.add("蔓越莓")
+        fruits.add("草莓")
+        fruits.add("枸杞")
+        fruits.add("葡萄柚")
+        fruits.add("火龙果")
+        fruits.add("菠萝蜜")
+        fruits.add("奇异果")
+        fruits.add("杨桃")
+        fruits.add("柠檬")
+        fruits.add("青柠")
+        fruits.add("榴莲")
+        fruits.add("荔枝")
+        fruits.add("芒果")
+        fruits.add("牛油果")
+        fruits.add("椰子")
+        fruits.add("哈密瓜")
+        fruits.add("橄榄")
+        fruits.add("木瓜")
+        fruits.add("山竹")
+    }
+}
