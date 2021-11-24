71cd341da06a7d108d705d71a85bb22d6bad7f6b
xuyingjun <xuyingjun@yy.com>
2021-11-24 15:30:43 +0800

    1.0.7

--- app/src/main/java/com/hensen/easylibrary/MainActivity.kt
+++ app/src/main/java/com/hensen/easylibrary/MainActivity.kt
@@ -1,14 +1,37 @@
 package com.hensen.easylibrary
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.hensen.easyutils.junjunjun1.ToastUtils
+import com.hensen.easyutils.junjunjun1.activity.HomeActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : HomeActivity() {
+    override fun getFragment(): List<Fragment> {
+        val l = mutableListOf<Fragment>()
+        l.add(Fragment())
+        l.add(Fragment())
+        l.add(Fragment())
+        return l
+    }
+
+    override fun getIcon(): List<Int> {
+        val l = mutableListOf<Int>()
+        l.add(R.drawable.icon_tab_home)
+        l.add(R.drawable.icon_tab_send)
+        l.add(R.drawable.icon_tab_me)
+        return l
+    }
+
+    override fun getTitlee(): List<String> {
+        val l = mutableListOf<String>()
+        l.add("你")
+        l.add("我")
+        l.add("他")
+        return l
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
         ToastUtils.getInstance().showToast("Hello")
     }
 }
---
+++ easyUtils/src/main/java/com/hensen/easyutils/junjunjun1/activity/HomeActivity.kt
@@ -0,0 +1,74 @@
+package com.hensen.easyutils.junjunjun1.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.hensen.easyutils.R
+
+abstract class HomeActivity : AppCompatActivity() {
+
+    private val fragment1: Fragment = getFragment()[0]
+    private val fragment2: Fragment = getFragment()[1]
+    private val fragment3: Fragment = getFragment()[2]
+
+    abstract fun getFragment(): List<Fragment>
+    abstract fun getIcon(): List<Int>
+    abstract fun getTitlee(): List<String>
+
+    private var viewPager: ViewPager? = null
+    private var navigation: BottomNavigationView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+
+        viewPager = findViewById(R.id.viewpager)
+        viewPager?.offscreenPageLimit = 3
+        viewPager?.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                navigation?.getMenu()?.getItem(position)?.setChecked(true)
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+        })
+
+        navigation = findViewById(R.id.navigation)
+
+        navigation?.menu?.findItem(R.id.navigation_item1)?.setIcon(getIcon()[0])
+        navigation?.menu?.findItem(R.id.navigation_item2)?.setIcon(getIcon()[1])
+        navigation?.menu?.findItem(R.id.navigation_item3)?.setIcon(getIcon()[2])
+        navigation?.menu?.findItem(R.id.navigation_item1)?.setTitle(getTitlee()[0])
+        navigation?.menu?.findItem(R.id.navigation_item2)?.setTitle(getTitlee()[1])
+        navigation?.menu?.findItem(R.id.navigation_item3)?.setTitle(getTitlee()[2])
+
+        navigation?.setOnNavigationItemSelectedListener { item ->
+            viewPager?.setCurrentItem(item.getOrder())
+            true
+        }
+
+        viewPager?.setAdapter(object : FragmentPagerAdapter(supportFragmentManager) {
+            override fun getItem(position: Int): Fragment {
+                when (position) {
+                    0 -> return fragment1
+                    1 -> return fragment2
+                    2 -> return fragment3
+                }
+                return fragment1
+            }
+
+            override fun getCount(): Int {
+                return 3
+            }
+        })
+    }
+}
