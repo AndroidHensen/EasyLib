0b32599f5ab778a9adfac8802c9a78158a8afb86
xuyingjun <xuyingjun@yy.com>
2021-12-13 11:29:30 +0800

    1.0.16

---
+++ easyUtils/src/main/java/com/hensen/easyutils/junjunjun1/DateUtils.java
@@ -0,0 +1,42 @@
+package com.hensen.easyutils.junjunjun1;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+public class DateUtils {
+
+    //判断选择的日期是否是今天
+    public static boolean isToday(long time) {
+        return isThisTime(time, "yyyy-MM-dd");
+    }
+
+    //判断选择的日期是否是本周
+    public static boolean isThisWeek(long time) {
+        Calendar calendar = Calendar.getInstance();
+        int currentWeek = calendar.get(Calendar.WEEK_OF_YEAR);
+        calendar.setTime(new Date(time));
+        int paramWeek = calendar.get(Calendar.WEEK_OF_YEAR);
+        if (paramWeek == currentWeek) {
+            return true;
+        }
+        return false;
+    }
+
+    //判断选择的日期是否是本月
+    public static boolean isThisMonth(long time) {
+        return isThisTime(time, "yyyy-MM");
+    }
+
+    public static boolean isThisTime(long time, String pattern) {
+        Date date = new Date(time);
+        SimpleDateFormat sdf = new SimpleDateFormat(pattern, Locale.getDefault());
+        String param = sdf.format(date);//参数时间
+        String now = sdf.format(new Date());//当前时间
+        if (param.equals(now)) {
+            return true;
+        }
+        return false;
+    }
+}
